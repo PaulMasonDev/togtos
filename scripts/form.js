9197aed8a43f6b42b40baec18d09f38d5c11c2bc
Paul Mason <webdevpaulmason@gmail.com>
2022-12-03 21:59:56 -0800

    Push to test API key on netlify

--- scripts/form.js
+++ scripts/form.js
@@ -1,5 +1,10 @@
+import TMDB_API_KEY from "./apikey.js";
+// https://www.youtube.com/watch?v=2J3xbMkH2K4
+
 const form = document.getElementById("search-form");
 
 const handleFormSubmit = (event) => event.preventDefault();
 
 form.addEventListener("submit", handleFormSubmit);
+
+console.log(TMDB_API_KEY);
